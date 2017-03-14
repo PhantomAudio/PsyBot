edc5adb187514936cbf6182ab8640459acd3b156
PhantomAudio <phantomaudio.fortheworld@gmail.com>
2017-03-14 12:53:25 -0600

    Create actions.js

---
+++ src/actions.js
@@ -0,0 +1,53 @@
+import { MongoClient } from 'mongodb';
+import config from './config';
+
+export default class Actions {
+  // @TODO Check if the subscription exists and return that.
+  static add(repo, channelId) {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(config.db, (err, db) => {
+        if (err) reject(err);
+        db.collection('subscriptions').deleteMany({
+          'repo': repo.toLowerCase(),
+          'channelId': channelId
+        }, (err, result) => {
+          if (err) reject(err);
+          db.collection('subscriptions').insertOne({
+            repo: repo.toLowerCase(),
+            channelId: channelId
+          }, (err, result) => {
+            if (err) reject(err);
+            db.close();
+            resolve(`Added a new subscription. ${repo} <---> ${channelId}`);
+          });
+        });
+      });
+    });
+  }
+
+  static remove(repo, channelId) {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(config.db, (err, db) => {
+        if (err) reject(err);
+        db.collection('subscriptions').deleteOne({
+          'repo': repo.toLowerCase(),
+          'channelId': channelId
+        }, (err, result) => {
+          if (err) reject(err);
+          db.close();
+          resolve(`Removed a subscription. ${repo} <-/-> ${channelId}`);
+        });
+      });
+    });
+  }
+
+  static help(channel) {
+    const helpMessage = `\`\`\`
+Usage: !dbg <command> [value]
+Commands:
+  add <repo> ....... adds a subscription for the current channel
+  remove <repo> .... removes a subscription for the current channel
+  help ............. displays this text\`\`\``;
+    channel.sendMessage(helpMessage);
+  }
+}
